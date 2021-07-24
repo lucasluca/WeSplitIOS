47161bc8f843c48b81bd1feb4ac79b56e04f2845
Lucas Mendes <lucas.augustus@gmail.com>
2021-07-24 16:55:30 -0300

    Created some forms and loop interativo

--- WeSplitIOS/ContentView.swift
+++ WeSplitIOS/ContentView.swift
@@ -11,6 +11,9 @@ struct ContentView: View {
     
     @State private var tapCount = 0
     @State private var userName = ""
+    var students = ["Harry", "Hermione", "Ron"]
+    @State private var selectedStudent = "Harry"
+    
     
     var body: some View {
         NavigationView {
@@ -19,12 +22,26 @@ struct ContentView: View {
                     TextField("Insert your name", text: $userName)
                     Text("Your name is \(userName)" )
                 }
-                .navigationBarTitle("Swift UI")
+                
+                Form {
+                    ForEach(1 ..< 101) { number in
+                        Text("Number \(number)")
+                    }
+                }
+                Form {
+                    Picker("Select a studend", selection: $selectedStudent) {
+                        ForEach(0 ..< students.count) { number in
+                            Text(self.students[number])
+                        }
+                    }
+                    Text("Selected student: \(selectedStudent)")
+                }
                 
                 Button("Clicked \(tapCount) times") {
                     tapCount += 1
                 }
             }
+            .navigationBarTitle("Swift UI")
            
         }
         
