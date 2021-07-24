cb0a6398ee6937cc65449451b8d81c2d944a1693
Lucas Mendes <lucas.augustus@gmail.com>
2021-07-24 16:34:40 -0300

    Created simple two way data binding

--- WeSplitIOS/ContentView.swift
+++ WeSplitIOS/ContentView.swift
@@ -8,9 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var tapCount = 0
+    @State private var userName = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                Form {
+                    TextField("Insert your name", text: $userName)
+                    Text("Your name is \(userName)" )
+                }
+                .navigationBarTitle("Swift UI")
+                
+                Button("Clicked \(tapCount) times") {
+                    tapCount += 1
+                }
+            }
+           
+        }
+        
     }
 }
 
