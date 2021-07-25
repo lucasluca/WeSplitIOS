8f03bdfdbf6b292ea2c1615d172f7c496e355434
Lucas Mendes <lucas.augustus@gmail.com>
2021-07-25 15:06:30 -0300

    Added a different style of picker

--- WeSplitIOS/ContentView.swift
+++ WeSplitIOS/ContentView.swift
@@ -10,23 +10,42 @@ import SwiftUI
 struct ContentView: View {
     
   
-    @State private var tipValue = ""
+    @State private var checkAmount = ""
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 2
     
-    
+    let tipPercentages = [10, 15, 20, 25, 0]
+
     var body: some View {
         NavigationView {
             Form {
                 Section {
-                    TextField("Insira um valor", text: $tipValue)
+                    TextField("Insert a value", text: $checkAmount)
                         .keyboardType(.decimalPad)
                 }
                 Section {
-                    Text("$\(tipValue)")
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2 ..< 100) { quantityOfPeople in
+                            Text("\(quantityOfPeople) people")
+                        }
+                    }
+                }
+                Section(header: Text("How much tip do you want to leave?")) {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(0 ..< tipPercentages.count) { index in
+                            Text("\(self.tipPercentages[index])%")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+                Section {
+                    Text("$\(checkAmount)")
                 }
             }
             .navigationBarTitle("We Split")
            
         }
+        
         
     }
 }
